07ea33b33a9ddeb59bf66716385271d76fa3f75c
josesilvestrefernando-sketch <josesilvestrefernando@gmail.com>
2026-02-10 13:52:09 +0800

    Add files via upload

--- src/Main.java
+++ src/Main.java
@@ -1,1 +1,9 @@
+import roundrobintournament.*;
 
+
+void main() throws IOException {
+    RoundRobinTournamentApp tournament = new RoundRobinTournamentApp();
+    tournament.run();
+    //RoundRobinTournament roundRobinTournament=new RoundRobinTournament();
+   // roundRobinTournament.loadRecord("record.csv");
+}
